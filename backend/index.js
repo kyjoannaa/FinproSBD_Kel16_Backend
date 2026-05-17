37836c52aa57f0868587b4b95684ad89ab0b1f72
finnamrn <amaranisafina@gmail.com>
2026-05-17 21:56:40 +0700

    ganti redis

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const { connectRedis } = require('./src/config/redis')
 require('dotenv').config();
 
 const app = express();
@@ -10,10 +9,8 @@ app.use(cors({
     origin: '*'
 }))
 app.use(express.json()); 
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use('/api/upload', require('./src/routes/upload'))
-
 app.use('/api/auth', require('./src/routes/authRoutes'));
 app.use('/api/messages', require('./src/routes/messageRoutes'));
 
@@ -22,9 +19,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
-connectRedis()
-
 app.listen(PORT, () => {
     console.log(`Server EchoDrop berjalan di port ${PORT}`);
 });
